edad8513759c92455862a5c99099f4f9da42d697
Weidong <xuwd11@gmail.com>
2019-05-03 21:49:38 -0400

    add constants

--- 07_Proxy_Lab/proxylab-handout/proxy.c
+++ 07_Proxy_Lab/proxylab-handout/proxy.c
@@ -4,6 +4,11 @@
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
+#define MAX_HOSTNAME_LEN 64
+#define MAX_PORT_LEN 20
+#define MAX_HEADERNAME_LEN 32
+#define MAX_HEADER_LEN 64
+
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
